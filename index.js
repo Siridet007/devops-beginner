88fce312f9d32f1e6512f5ddb905218f7575b385
Siridet007 <siridet.wo1996@gmail.com>
2026-04-05 20:50:31 +0700

    fix port for render

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const PORT = process.env.PORT || 3000
 
 http.createServer((req,res)=>{
     res.end('Hello DevOps CI/CD 🚀');
-}).listen(3000);
+}).listen(PORT);
 
-console.log('Server running on port 3000');
+console.log('Server running on port '+PORT);
