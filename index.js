e469db7cc37ec53bcf47a461f2c43ceafe22d6a7
Siridet007 <siridet.wo1996@gmail.com>
2026-04-05 22:23:51 +0700

    Test Webhook Fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000
 
 http.createServer((req,res)=>{
-    res.end('Auto Deploy Final Test 🚀');
+    res.end('Webhook Fix Test 🚀');
 }).listen(PORT);
 
 console.log('Server running on port '+PORT);
