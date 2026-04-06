2dca8eebd704aec296d21f43e0c241a1fb555702
Siridet007 <siridet.wo1996@gmail.com>
2026-04-06 14:59:34 +0700

    add request counter

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const PORT = process.env.PORT || 3000
 
+let count = 0;
+
 http.createServer((req,res)=>{
+    count++;
     console.log("Request received:",req.url);
+    console.log(`Request #${count}: ${req.url}`);
 
     res.end('Loging Working 🚀');
 }).listen(PORT);
