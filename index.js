8010cffd5d64fae7ea992b8c395c3a7670c16221
Siridet007 <siridet.wo1996@gmail.com>
2026-04-06 19:47:38 +0700

    version 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const PORT = process.env.PORT || 3000
+const HOSTNAME = process.env.HOSTNAME;
 
 let count = 0;
 
@@ -7,8 +8,13 @@ http.createServer((req,res)=>{
     count++;
     console.log("Request received:",req.url);
     console.log(`Request #${count}: ${req.url}`);
+    console.log("Request from:",HOSTNAME);
 
-    res.end('Loging Working 🚀');
+    res.writeHead(200,{
+        "Content-Type":"text/plain; charset=utf-8",
+    });
+
+    res.end(`Version 2 from : ${HOSTNAME} 🚀`);
 }).listen(PORT);
 
 console.log('Server running on port '+PORT);
